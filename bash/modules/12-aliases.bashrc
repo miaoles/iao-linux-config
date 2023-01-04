0929d50595e0c7dc3384ff379be45a018ce2787d
miaoles <iao_mm@pm.me>
2023-01-04 10:18:06 -0500

    january update

--- bash/modules/12-aliases.bashrc
+++ bash/modules/12-aliases.bashrc
@@ -4,3 +4,4 @@ alias ls="ls --color=auto"
 alias sxlock='sxlock -f "-*-roboto-*-r-*-*-*-*-*-*-*-*-*-*"'
 alias steam='steam -console'
 alias priaoritize="ionice -c 2 -n 0"
+alias godot4="steam-run ~/Executables/Godot/Godot_v4.0-beta2_linux.x86_64 &"
--- bspwm/rule-correction.sh
+++ bspwm/rule-correction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # bspwm rule correction
 
-sleep 12;
+sleep 30;
 
 bspc rule -a pcmanfm-qt desktop=active
 bspc rule -a qterminal desktop=active
@@ -9,3 +9,8 @@ bspc rule -a FeatherPad desktop=active
 bspc rule -a Nightly desktop=active
 bspc rule -a Firefox desktop=active
 bspc rule -a Navigator desktop=active
+bspc rule -a chatterino2 desktop=active
+bspc rule -a chatterino desktop=active
+bspc rule -a chatterino7 desktop=active
+bspc rule -a discord desktop=active
+bspc rule -a discord-canary desktop=active
